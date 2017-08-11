5315364944a7d1f8ab22913b83b02df59418c8b6
johney <johney.zhao@carbit.com.cn>
2017-08-11 09:44:26 +0800

    初始化代码

---
+++ index.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+
+from util.util import *
+
+
+def main():
+    get_titles("")
+    dl("")
+    pass
+
+main()
---
+++ util/util.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+def get_titles(file):
+    print "hello get_titles"
+    pass
+
+
+def dl(paper_title):
+    print "hello paper_title"
+    pass
+
+
